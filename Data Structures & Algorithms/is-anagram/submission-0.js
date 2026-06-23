f4e3a52233bfd57341a4b9e3f980c5ef413da6e6
SonaliMhatre18 <99874393+SonaliMhatre18@users.noreply.github.com>
2026-06-23 17:59:01 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,12 @@
+class Solution{
+
+    isAnagram(s,t){
+        if(s.length !== t.length){
+            return false;
+        }
+        let sSort = s.split('').sort().join('');
+        let tSort = t.split('').sort().join('');
+
+        return sSort == tSort;
+    }
+}
